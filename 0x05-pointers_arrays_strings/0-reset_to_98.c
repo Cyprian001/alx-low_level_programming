21f097f706049433eaa56b087f1602a1e3fed065
NZEKWE Chinonso Cyprian <nzekwecyprian@gmail.com>
2023-02-27 09:35:52 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * Description: reset_to_98 - function that takes a pointer to a parameter and update it value
+ * @x: int pointer
+ * Return: void
+ */
+
+void reset_to_98(int *x)
+{
+	*x = 98;
+}
