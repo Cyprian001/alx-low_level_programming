ed0d60d0959f19877dd22de025016a00a74933cb
NZEKWE Chinonso Cyprian <nzekwecyprian@gmail.com>
2023-04-14 21:21:55 -0700

    File I/O_file_input/output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+ *  * read_textfile - funtn that reads a text file and prints it
+ *   * to POSIX std o/p
+ *    * @letters: num of letters that needs to read and print
+ *     * @filename: filename to be executed.
+ *      * definitio: if filename is NULL || if write fails or does not
+ *       * if the file can not be opened or read, return 0
+ *        * Return: if filename is NULL return 0
+ *         * if write fails or does not write the expected amount of bytes, return 0
+ */
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+	free(buf);
+
+	return (nwr);
+
+}
